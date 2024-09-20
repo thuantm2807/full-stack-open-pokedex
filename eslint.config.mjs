8d71b84f92a17ecaf3f63e7575d5ffc2378c181b
Thomas <thuantm2807@gmail.com>
2024-09-20 17:13:33 +0700

    add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,6 @@
-import globals from 'globals'
-import stylisticJs from '@stylistic/eslint-plugin-js'
-import js from '@eslint/js'
+import globals from "globals";
+
 
 export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-      },
-      ecmaVersion: 'latest',
-    },
-    plugins: {
-      '@stylistic/js': stylisticJs,
-    },
-    rules: {
-      '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
-      eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'off',
-    },
-  },
-  {
-    ignores: ['dist/**', 'build/**'],
-  },
-]
+  {languageOptions: { globals: globals.browser }},
+];
